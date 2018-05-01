616db767363ca3f9fe0c8a7ec734185a57a02d17
Aleksbgbg <kozichka01@abv.bg>
2018-05-01 15:47:39 +0100

    Add QuickSort algorithm.

---
+++ Sorting Algorithms/Core/Sort/QuickSort.cs
@@ -0,0 +1,41 @@
+﻿namespace Sorting.Algorithms.Core.Sort
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Sorting.Algorithms.Core.Interfaces;
+
+    internal class QuickSort<T> : ISortingAlgorithm<T>
+    {
+        public T[] Sort(IEnumerable<T> elements)
+        {
+            T[] array = elements.ToArray();
+
+            if (array.Length < 2)
+            {
+                return array;
+            }
+
+            T pivot = array.Last();
+
+            List<T> left = new List<T>();
+            List<T> right = new List<T>();
+
+            foreach (T element in array)
+            {
+                int comparison = Comparer<T>.Default.Compare(element, pivot);
+
+                if (comparison == -1)
+                {
+                    left.Add(element);
+                }
+                else if (comparison == 1)
+                {
+                    right.Add(element);
+                }
+            }
+
+            return Sort(left).Append(pivot).Union(Sort(right)).ToArray();
+        }
+    }
+}
