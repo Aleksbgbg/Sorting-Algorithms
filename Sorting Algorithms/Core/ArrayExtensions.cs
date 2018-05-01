806b42ac240920e2e089540d42f1ffe572c79dfd
Aleksbgbg <kozichka01@abv.bg>
2018-05-01 19:01:37 +0100

    Add ArrayExtensions class with Swap<T> method to swap elements in an array.

---
+++ Sorting Algorithms/Core/ArrayExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Sorting.Algorithms.Core
+{
+    internal static class ArrayExtensions
+    {
+        internal static void Swap<T>(this T[] array, int indexFirst, int indexSecond)
+        {
+            T temporary = array[indexFirst];
+            array[indexFirst] = array[indexSecond];
+            array[indexSecond] = temporary;
+        }
+    }
+}
