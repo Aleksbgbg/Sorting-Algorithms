163a059ce83b2bf967c9a95c61f168c6326ab051
Aleksbgbg <kozichka01@abv.bg>
2018-05-01 17:49:31 +0100

    Add EnumerableExtensions for easy collection string joining.

---
+++ Sorting Algorithms/Core/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Sorting.Algorithms.Core
+{
+    using System.Collections.Generic;
+
+    internal static class EnumerableExtensions
+    {
+        internal static string Join<T>(this IEnumerable<T> collection, string separator)
+        {
+            return string.Join(separator, collection);
+        }
+
+        internal static string JoinWithSpace<T>(this IEnumerable<T> collection)
+        {
+            return collection.Join(" ");
+        }
+    }
+}
