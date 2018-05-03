5ba9a72de5cc2b55838113f32ef866da025d1bb3
Aleksbgbg <kozichka01@abv.bg>
2018-05-03 07:36:22 +0100

    [ArrayExtensions] Add ShiftElement extension to move elements around in arrays.

--- Sorting Algorithms/Core/Extensions/ArrayExtensions.cs
+++ Sorting Algorithms/Core/Extensions/ArrayExtensions.cs
@@ -1,5 +1,7 @@
 ﻿namespace Sorting.Algorithms.Core.Extensions
 {
+    using System;
+
     internal static class ArrayExtensions
     {
         internal static void Swap<T>(this T[] array, int indexFirst, int indexSecond)
@@ -7,6 +9,24 @@
             T temporary = array[indexFirst];
             array[indexFirst] = array[indexSecond];
             array[indexSecond] = temporary;
+        }
+
+        internal static void ShiftElement<T>(this T[] array, int oldIndex, int newIndex)
+        {
+            T temporary = array[oldIndex];
+
+            if (newIndex < oldIndex)
+            {
+                // Need to move part of the array "up" to make room
+                Array.Copy(array, newIndex, array, newIndex + 1, oldIndex - newIndex);
+            }
+            else
+            {
+                // Need to move part of the array "down" to fill the gap
+                Array.Copy(array, oldIndex + 1, array, oldIndex, newIndex - oldIndex);
+            }
+
+            array[newIndex] = temporary;
         }
     }
 }
